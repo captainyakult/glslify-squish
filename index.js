b73364326f0a5f6c5f71e1bdbe5da985f5eb4c14
Jack Simpson <jack@mjdinteractive.com>
2020-03-17 22:51:41 +0100

    basic bitch transform func, package test script update

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-L
+
+
+module.exports = transform;
+module.exports.sync = transform;
+
+function transform(filename, src, opts, done) {
+
+  console.log('\nWe are inside the GLSL pipe!');
+  console.log('filename', filename);
+  console.log('src', src);
+  console.log('opts', opts);
+  console.log('done', done);
+
+  // src = src.replace(regexLong, function(whole, r, g, b, a) {
+  //   return makeVec(r, g, b, a)
+  // });
+
+  if (typeof done === 'function') done(null, src);
+  return src
+}
