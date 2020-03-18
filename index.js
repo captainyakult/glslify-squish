673dae7ad377d743b0aabac002e0a62865a968bc
Jack Simpson <jack@mjdinteractive.com>
2020-03-18 12:43:34 +0100

    installed other dependencies, testing tokenizer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const tokenizer = require('glsl-tokenizer/string');
 
-module.exports = transform;
-module.exports.sync = transform;
+import tokenizer from 'glsl-tokenizer/stream'
+import parser from 'glsl-parser';
+import minify from 'glsl-min-stream';
+import deparser from 'glsl-deparser';
+import stringToStream from 'string-to-stream';
 
-function transform(filename, src, opts, done) {
+
+const transform = (filename, src, opts, done) => {
 
   console.log('\nWe are inside the GLSL pipe!');
   console.log('filename', filename);
@@ -11,9 +14,18 @@ function transform(filename, src, opts, done) {
   console.log('opts', opts);
   console.log('done', done);
 
-  const tokens = tokenizer(src);
+  const tokens = stringToStream(src)
+    .pipe(tokenizer())
+    .on('error', error => console.error(`Tokenizer: ${error}`));
 
-  console.log('tokens', tokens);
+  console.log(tokens);
+
+  // .pipe(parser())
+  //   .on('error', error => console.error(`Parser: ${error}`))
+  //   .pipe(minify())
+  //   .on('error', error => console.error(`Minify: ${error}`))
+  //   .pipe(deparser(false))
+  //   .on('error', error => console.error(`Deparser: ${error}`))
 
   // src = src.replace(regexLong, function(whole, r, g, b, a) {
   //   return makeVec(r, g, b, a)
@@ -21,4 +33,7 @@ function transform(filename, src, opts, done) {
 
   if (typeof done === 'function') done(null, src);
   return src
-}
+};
+
+module.exports = transform;
+module.exports.sync = transform;
